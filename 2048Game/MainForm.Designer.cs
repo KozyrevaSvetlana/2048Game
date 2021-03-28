149571741a20ba5b253ef198b4c647a25eb654a8
LanaKo0507 <lanako0507@gmail.com>
2021-03-28 11:58:01 +0300

    Add project files.

---
+++ 2048Game/MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace _2048Game
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scoreTextLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.статистикаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рестартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходИзИгрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правилаИгрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bestScoreTextLabel = new System.Windows.Forms.Label();
+            this.bestScoreNumberLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // scoreTextLabel
+            // 
+            this.scoreTextLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.scoreTextLabel.Location = new System.Drawing.Point(12, 24);
+            this.scoreTextLabel.Name = "scoreTextLabel";
+            this.scoreTextLabel.Size = new System.Drawing.Size(117, 40);
+            this.scoreTextLabel.TabIndex = 0;
+            this.scoreTextLabel.Text = "Счет:";
+            this.scoreTextLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.scoreLabel.Location = new System.Drawing.Point(62, 45);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(13, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.правкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(780, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.статистикаToolStripMenuItem,
+            this.рестартToolStripMenuItem,
+            this.выходИзИгрыToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // статистикаToolStripMenuItem
+            // 
+            this.статистикаToolStripMenuItem.Name = "статистикаToolStripMenuItem";
+            this.статистикаToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.статистикаToolStripMenuItem.Text = "Статистика";
+            // 
+            // рестартToolStripMenuItem
+            // 
+            this.рестартToolStripMenuItem.Name = "рестартToolStripMenuItem";
+            this.рестартToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.рестартToolStripMenuItem.Text = "Рестарт";
+            this.рестартToolStripMenuItem.Click += new System.EventHandler(this.рестартToolStripMenuItem_Click);
+            // 
+            // выходИзИгрыToolStripMenuItem
+            // 
+            this.выходИзИгрыToolStripMenuItem.Name = "выходИзИгрыToolStripMenuItem";
+            this.выходИзИгрыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходИзИгрыToolStripMenuItem.Text = "Выход из игры";
+            this.выходИзИгрыToolStripMenuItem.Click += new System.EventHandler(this.выходИзИгрыToolStripMenuItem_Click);
+            // 
+            // правкаToolStripMenuItem
+            // 
+            this.правкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.правилаИгрыToolStripMenuItem});
+            this.правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            this.правкаToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // правилаИгрыToolStripMenuItem
+            // 
+            this.правилаИгрыToolStripMenuItem.Name = "правилаИгрыToolStripMenuItem";
+            this.правилаИгрыToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.правилаИгрыToolStripMenuItem.Text = "Правила игры";
+            // 
+            // bestScoreTextLabel
+            // 
+            this.bestScoreTextLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.bestScoreTextLabel.Location = new System.Drawing.Point(149, 24);
+            this.bestScoreTextLabel.Name = "bestScoreTextLabel";
+            this.bestScoreTextLabel.Size = new System.Drawing.Size(117, 40);
+            this.bestScoreTextLabel.TabIndex = 3;
+            this.bestScoreTextLabel.Text = "Лучший счет:";
+            this.bestScoreTextLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // bestScoreNumberLabel
+            // 
+            this.bestScoreNumberLabel.AutoSize = true;
+            this.bestScoreNumberLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.bestScoreNumberLabel.Location = new System.Drawing.Point(200, 45);
+            this.bestScoreNumberLabel.Name = "bestScoreNumberLabel";
+            this.bestScoreNumberLabel.Size = new System.Drawing.Size(13, 13);
+            this.bestScoreNumberLabel.TabIndex = 4;
+            this.bestScoreNumberLabel.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 506);
+            this.Controls.Add(this.bestScoreNumberLabel);
+            this.Controls.Add(this.bestScoreTextLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.scoreTextLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Игра 2048";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreTextLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem статистикаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рестартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходИзИгрыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правилаИгрыToolStripMenuItem;
+        private System.Windows.Forms.Label bestScoreTextLabel;
+        private System.Windows.Forms.Label bestScoreNumberLabel;
+    }
+}
+
---
+++ 2048Game/MainForm.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048Game
+{
+    public partial class MainForm : Form
+    {
+        private int score = 0;
+        private int bestScore;
+        private string bestScorePath = "bestscore.txt";
+        public const int mapSize = 4;
+        private Label[,] labelsMap;
+        private static Random random = new Random();
+
+        private bool victory = false;
+        public MainForm()
+        {
+            InitializeComponent();
+            ClientSize = new Size(76 * mapSize + 12, 76 * mapSize + 75);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bestScore = GetBestScore();
+            ShowScore();
+            InitMap();
+            GenerateNumber();
+
+        }
+
+        private int GetBestScore()
+        {
+            if (File.Exists(bestScorePath))
+            {
+                var reader = new StreamReader(bestScorePath);
+                var bestScore = Convert.ToInt32(reader.ReadLine());
+                reader.Close();
+                return bestScore;
+
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        private void ShowScore()
+        {
+            scoreLabel.Text = score.ToString();
+            if (score> bestScore)
+            {
+                bestScore = score;
+            }
+            bestScoreNumberLabel.Text = bestScore.ToString();
+        }
+
+
+
+
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize, mapSize];
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel(i, j);
+                    Controls.Add(newLabel);
+                    labelsMap[i, j] = newLabel;
+                }
+            }
+        }
+
+        private Label CreateLabel(int indexRow, int indexColumn)
+        {
+            var label = new Label();
+            label.BackColor = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(251)))), ((int)(((byte)(232)))));
+            label.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(204)));
+            label.Size = new Size(70, 70);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            int x = 10 + indexColumn * 76;
+            int y = 70 + indexRow * 76;
+            label.Location = new Point(x, y);
+            label.TextChanged += Label_TextChanged;
+            return label;
+        }
+
+        private void Label_TextChanged(object sender, EventArgs e)
+        {
+            var label = (Label)sender;
+            UpdateLabelBackColor(label);
+            if (label.Text=="2048")
+            {
+                victory = true;
+            }
+
+        }
+
+        private void GenerateNumber()
+        {
+            while (true)
+            {
+                var randomNumberLabel = random.Next(mapSize * mapSize);
+                var indexRow = randomNumberLabel / mapSize;
+                var indexColumn = randomNumberLabel % mapSize;
+                var randomNumber = random.Next(1,5);
+                randomNumber = randomNumber < 4 ? 2 : 4;
+                if (labelsMap[indexRow, indexColumn].Text == string.Empty)
+                {
+                    labelsMap[indexRow, indexColumn].Text = randomNumber.ToString();
+                    UpdateLabelBackColor(labelsMap[indexRow, indexColumn]);
+                    break;
+                }
+            }
+
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Right && e.KeyCode != Keys.Left && e.KeyCode != Keys.Up && e.KeyCode != Keys.Down)
+            {
+                return;
+            }
+
+            bool isMerged = false;
+            if (e.KeyCode == Keys.Right)
+            {
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = mapSize - 1; j >= 0; j--)
+                    {
+                        if (labelsMap[i, j].Text != string.Empty)
+                        {
+                            for (int k = j - 1; k >= 0; k--)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                    {
+                                        var number = int.Parse(labelsMap[i, j].Text);
+                                        score += number*2;
+                                        labelsMap[i, j].Text = (number * 2).ToString();
+                                        labelsMap[i, k].Text = string.Empty;
+                                        isMerged = true;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = mapSize - 1; j >= 0; j--)
+                    {
+                        if (labelsMap[i, j].Text == string.Empty)
+                        {
+                            for (int k = j - 1; k >= 0; k--)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                    labelsMap[i, k].Text = string.Empty;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        if (labelsMap[i, j].Text != string.Empty)
+                        {
+                            for (int k = j + 1; k < mapSize; k++)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                    {
+                                        var number = int.Parse(labelsMap[i, j].Text);
+                                        score += number * 2;
+                                        labelsMap[i, j].Text = (number * 2).ToString();
+                                        labelsMap[i, k].Text = string.Empty;
+                                        isMerged = true;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < mapSize; i++)
+                {
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        if (labelsMap[i, j].Text == string.Empty)
+                        {
+                            for (int k = j + 1; k < mapSize; k++)
+                            {
+                                if (labelsMap[i, k].Text != string.Empty)
+                                {
+                                    labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                    labelsMap[i, k].Text = string.Empty;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    for (int i = 0; i < mapSize; i++)
+                    {
+                        if (labelsMap[i, j].Text != string.Empty)
+                        {
+                            for (int k = i + 1; k < mapSize; k++)
+                            {
+                                if (labelsMap[k, j].Text != string.Empty)
+                                {
+                                    if (labelsMap[i, j].Text == labelsMap[k, j].Text)
+                                    {
+                                        var number = int.Parse(labelsMap[i, j].Text);
+                                        score += number * 2;
+                                        labelsMap[i, j].Text = (number * 2).ToString();
+                                        labelsMap[k, j].Text = string.Empty;
+                                        isMerged = true;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < mapSize; j++)
+                {
+                    for (int i = 0; i < mapSize; i++)
+                    {
+                        if (labelsMap[i, j].Text == string.Empty)
+                        {
+                            for (int k = i + 1; k < mapSize; k++)
+                            {
+                                if (labelsMap[k, j].Text != string.Empty)
+                                {
+                                    labelsMap[i, j].Text = labelsMap[k, j].Text;
+                                    labelsMap[k, j].Text = string.Empty;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    for (int i = mapSize-1; i >=0 ; i--)
+                    {
+                        if (labelsMap[i, j].Text != string.Empty)
+                        {
+                            for (int k = i - 1; k >=0; k--)
+                            {
+                                if (labelsMap[k, j].Text != string.Empty)
+                                {
+                                    if (labelsMap[i, j].Text == labelsMap[k, j].Text)
+                                    {
+                                        var number = int.Parse(labelsMap[i, j].Text);
+                                        score += number * 2;
+                                        labelsMap[i, j].Text = (number * 2).ToString();
+                                        labelsMap[k, j].Text = string.Empty;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < mapSize; j++)
+                {
+                    for (int i = mapSize - 1; i >= 0; i--)
+                    {
+                        if (labelsMap[i, j].Text == string.Empty)
+                        {
+                            for (int k = i - 1; k >= 0; k--)
+                            {
+                                if (labelsMap[k, j].Text != string.Empty)
+                                {
+                                    labelsMap[i, j].Text = labelsMap[k, j].Text;
+                                    labelsMap[k, j].Text = string.Empty;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            ShowScore();
+            if (IsFullMap()&& !isMerged)
+            {
+                MessageBox.Show($"Игра окончена. Ваш счет: {score}");
+            }
+            if (victory)
+            {
+                MessageBox.Show($"Поздравляем! Вы набрали 2048 очков. Ваш счет: {score}");
+            }
+            GenerateNumber();
+
+        }
+
+        private void UpdateLabelBackColor(Label label)
+        {
+            int indicatorTwo = 0;
+            if (label.Text==string.Empty)
+            {
+                indicatorTwo = 0;
+
+            }
+            else
+            {
+                var number = Int32.Parse(label.Text);
+                indicatorTwo=(int)Math.Log(number, 2);
+            }
+
+            Color[] colors = new Color[12];
+            colors[0] = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(251)))), ((int)(((byte)(232)))));
+            colors[1] = Color.LemonChiffon;
+            colors[2] = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            colors[3] = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            colors[4] = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            colors[5] = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            colors[6] = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            colors[7] = Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            colors[8] = Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            colors[9] = Color.Thistle;
+            colors[10] = Color.Violet;
+            colors[11] = Color.Red;
+            label.BackColor = colors[indicatorTwo];
+        }
+        private bool IsFullMap()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (labelsMap[i,j].Text==string.Empty)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var writer = new StreamWriter(bestScorePath, false);
+            writer.WriteLine(bestScore);
+            writer.Close();
+        }
+
+        private void выходИзИгрыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите выйти из игры?", "Exit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void рестартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы хотите начать игру заново?", "Exit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Application.Restart();
+        }
+    }
+}
